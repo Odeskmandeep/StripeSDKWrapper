4f1c5243afb2f2cc440ef51f3e6ad0e5d9ff5176
Mandeep Kumar <odeskmandeepkumar@gmail.com>
2026-04-28 12:07:28 +0530

    latest work

--- StripeM2/src/main/java/singhsarae/badshah/stripem2/utilities/TokenProvider.kt
+++ StripeM2/src/main/java/singhsarae/badshah/stripem2/utilities/TokenProvider.kt
@@ -43,6 +43,8 @@ class TokenProvider() : ConnectionTokenProvider {
                     hashMap = reqBodyHash
                 )
                 if (response.isSuccessful) {
+                    Log.i("BADSHAH","response.body():${response.body()}")
+                    Log.i("BADSHAH"," response.body()?.secret:${ response.body()?.secret}")
                     val secret = response.body()?.secret
                     if (!secret.isNullOrEmpty()){
                         callback.onSuccess(secret)
