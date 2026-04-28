f45243db66f852d56dfbda3c293bd6b2c5abb6e1
Mandeep Kumar <odeskmandeepkumar@gmail.com>
2026-04-28 11:08:16 +0530

    latest work

--- StripeM2/build.gradle.kts
+++ StripeM2/build.gradle.kts
@@ -5,40 +5,53 @@ plugins {
 
 android {
     namespace = "singhsarae.badshah.stripem2"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 28
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "consumer-rules.pro"
+            )
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    // IMPORTANT: correct place for publishing config
     publishing {
         singleVariant("release") {
             withSourcesJar()
         }
     }
-
 }
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.18.0")
     implementation("com.google.android.material:material:1.13.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
-    //Stripe:
-    implementation("com.stripe:stripeterminal:5.3.0") //Stable version till date
+
+    // Stripe
+    implementation("com.stripe:stripeterminal:5.3.0")
+
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
@@ -46,13 +59,16 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
 
+/**
+ * Maven publishing (JitPack safe)
+ */
 afterEvaluate {
     publishing {
         publications {
             create<MavenPublication>("release") {
                 groupId = "com.github.Odeskmandeep"
                 artifactId = "StripeSDKWrapper"
-                version = "1.0.7"
+                version = "1.0.8"
 
                 from(components["release"])
             }
